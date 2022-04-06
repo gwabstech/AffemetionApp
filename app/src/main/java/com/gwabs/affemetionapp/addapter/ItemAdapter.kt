13c5b746d4d1b62110355d2c93b75078e6f320be
gwabstech <72021618+gwabstech@users.noreply.github.com>
2022-04-06 11:07:56 +0100

    in progress

--- app/src/main/java/com/gwabs/affemetionapp/addapter/ItemAdapter.kt
+++ app/src/main/java/com/gwabs/affemetionapp/addapter/ItemAdapter.kt
@@ -2,6 +2,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.gwabs.affemetionapp.R
@@ -21,6 +22,7 @@ class ItemAdapter(
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView : ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -40,6 +42,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
--- app/src/main/java/com/gwabs/affemetionapp/model/Affirmation.kt
+++ app/src/main/java/com/gwabs/affemetionapp/model/Affirmation.kt
@@ -1,4 +1,10 @@
 package com.gwabs.affemetionapp.model
 
-class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int) {
+
 }
